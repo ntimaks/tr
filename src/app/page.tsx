44a325d431139d69939d2643950be26cb82d9766
ntimaks <ntimaks27@gmail.com>
2024-08-01 13:14:02 +0300

    sign in/out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany();
 
   return (
+    <div className="flex flex-wrap gap-20">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} />
+          <h2>{image.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-20">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <h2>{image.name}</h2>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="flex h-full w-full text-2xl">
+          Please sign in to view images
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
