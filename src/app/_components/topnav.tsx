8a4d66dc265b08283afd788d3ca8dbfa28755d8a
ntimaks <ntimaks27@gmail.com>
2024-08-14 13:03:50 +0300

    home button works

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,10 +1,11 @@
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./simple-upload-button";
+import Link from 'next/link'
 
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <Link href="/">Gallery</Link>
       <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
