126ed0b9a7397212848bf90430388b3256cb9da8
ntimaks <ntimaks27@gmail.com>
2024-08-14 14:16:14 +0300

    delete button

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,27 +1,45 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { Button } from "~/components/ui/button";
+import { deleteImage, getImage } from "~/server/queries";
 
-export default async function FullPageImageView(props: { photoid: number }) {
-  const image = await getImage(props.photoid);
-  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+export async function FullPageImageView(props: { photoId: string }) {
+  const idAsNumber = Number(props.photoId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
+
+  const userInfo = await clerkClient.users.getUser(image.userId);
 
   return (
-    <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
-        <img src={image.url} className="flex-shrink object-contain" />
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
+        <img src={image.url} className="object-contain" alt={image.name} />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="border-b p-2 text-center text-lg">{image.name}</div>
-        <div className="flex flex-col p-2">
-          <span>Uploaded by:</span>
-          <span>{uploaderInfo.fullName}</span>
+      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-xl">{image.name}</div>
+
+        <div className="p-2">
+          <div>Uploaded By:</div>
+          <div>{userInfo.fullName}</div>
         </div>
-        <div className="flex flex-col p-2">
-          <span>Created On:</span>
-          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
         </div>
-        <div className="flex flex-col p-2">
-          <button className="bg-neutral-900 text-white p-2 rounded">Delete</button>
+
+        <div className="p-2">
+          <form
+            action={async () => {
+              "use server";
+
+              await deleteImage(idAsNumber);
+            }}
+          >
+            <Button type="submit" variant="destructive">
+              Delete
+            </Button>
+          </form>
         </div>
       </div>
     </div>
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,15 +1,21 @@
 import "server-only";
-import { db } from "~/server/db";
+import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
+import { images } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
+
   if (!user.userId) throw new Error("Unauthorized");
 
   const images = await db.query.images.findMany({
     where: (model, { eq }) => eq(model.userId, user.userId),
     orderBy: (model, { desc }) => desc(model.id),
   });
+
   return images;
 }
 
@@ -20,9 +26,28 @@ export async function getImage(id: number) {
   const image = await db.query.images.findFirst({
     where: (model, { eq }) => eq(model.id, id),
   });
-
-  if (!image) throw new Error("Not found");
+  if (!image) throw new Error("Image not found");
 
   if (image.userId !== user.userId) throw new Error("Unauthorized");
+
   return image;
+}
+
+export async function deleteImage(id: number) {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(images)
+    .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
+
+  redirect("/");
 }
