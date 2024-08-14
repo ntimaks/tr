b665c9b192a66e6c2684aa080a27ebeb63f613ce
ntimaks <ntimaks27@gmail.com>
2024-08-14 15:54:59 +0300

    egg

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -21,10 +21,11 @@ export function Modal({ children }: { children: React.ReactNode }) {
   return createPortal(
     <dialog
       ref={dialogRef}
-      className="m-0 h-screen w-screen bg-black/90 text-white"
+      className="absolute h-screen w-screen bg-black/90"
       onClose={onDismiss}
     >
       {children}
+      {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
     document.getElementById("modal-root")!,
   );
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,11 +1,14 @@
-import FullPageImageView from "~/components/full-image-page";
+import { Modal } from "~/app/@modal/(.)img/[id]/modal";
+import { FullPageImageView } from "~/components/full-image-page";
+
 export default async function PhotoModal({
-  params: { id: photoid },
+  params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  const idAsNumber = Number(photoid);
-  if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
-
-  return <FullPageImageView photoid={idAsNumber} />;
+  return (
+    <Modal>
+      <FullPageImageView photoId={photoId} />
+    </Modal>
+  );
 }
