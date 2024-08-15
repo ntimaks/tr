af2a0a72f8e011cadefc553d15b7b42efd6d2425
ntimaks <ntimaks27@gmail.com>
2024-08-15 14:38:51 +0300

    fixy thingy

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,18 +1,14 @@
-import { getImage } from "~/server/queries";
-import FullPageImageView from "~/components/full-image-page";
 import { Modal } from "./modal";
+import { FullPageImageView } from "~/common/full-page-image-view";
+
 export default async function PhotoModal({
-  params: { id: photoid },
+  params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  const idAsNumber = Number(photoid);
-  if (Number.isNaN(idAsNumber)) throw new Error("Invalid ID");
-
-  const image = await getImage(idAsNumber);
   return (
     <Modal>
-      <FullPageImageView photoid={idAsNumber} />
+      <FullPageImageView photoId={photoId} />
     </Modal>
   );
 }
