246215141a209d3c9d5018a429cf16eccddf3bf1
ntimaks <ntimaks27@gmail.com>
2024-08-16 13:02:23 +0300

    kk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,19 @@ async function Images() {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
       {images.map((image) => (
         <div key={image.id} className="flex flex-col items-center">
-          <Link href={`/img/${image.id}`} className="w-full aspect-square relative">
-            <Image
-              src={image.url}
-              alt={image.name}
-              fill
-              sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, (max-width: 1280px) 25vw, 20vw"
-              className="object-contain"
-            />
+          <Link href={`/img/${image.id}`} className="w-full aspect-square relative group">
+            <div className="w-full h-full p-0.5 bg-[#213c2f] ">
+              {/* Added hover effects for scale and glow */}
+              <div className="relative w-full h-full">
+                <Image
+                  src={image.url}
+                  alt={image.name}
+                  fill
+                  sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, (max-width: 1280px) 25vw, 20vw"
+                  className="object-cover"
+                />
+              </div>
+            </div>
           </Link>
           <h2 className="mt-2 text-sm truncate w-full text-center">{image.name}</h2>
         </div>
